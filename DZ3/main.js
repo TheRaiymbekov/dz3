d180d9f63f152c27e2d3c67e1b1b7dcbb13c59c4
TheRaiymbekov <irajymbekov8@gmail.com>
2024-09-11 18:33:05 +0600

    Add files via upload

---
+++ DZ3/main.js
@@ -0,0 +1,34 @@
+// ЗАДАНИЕ 1
+// var num = (a,b) => {
+//     if(a>b) {
+//         console.log(b);
+//     }else if(a<b){
+//         console.log(a);
+//     }
+// }
+
+// num(45,56)
+
+// ЗАДАНИЕ 2
+// function init() {
+//     var name = "задание 2"
+    
+//     function displayName (){
+//         alert(name);
+//         console.log(name);
+//     }
+//     displayName();
+// }
+
+// init()
+
+// ЗАДАНИЕ 3 
+// var calc = (a,b,c,d,e,f,g,h) =>{
+//     console.log(a*b);
+//     console.log(c/d);
+//     console.log(e+f);
+//     console.log(g-h);
+// };
+
+// calc(18,7,75,5,34,28,80,21);
+
